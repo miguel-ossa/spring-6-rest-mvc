5cbd54bd3589ee33aabf27198f049533d9741358
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-03-31 17:20:12 +0100

    HTTP POST with Spring MVC

--- src/main/java/guru_springframework/spring_6_rest_mvc/controllers/BeerController.java
+++ src/main/java/guru_springframework/spring_6_rest_mvc/controllers/BeerController.java
@@ -4,10 +4,9 @@ import guru_springframework.spring_6_rest_mvc.model.Beer;
 import guru_springframework.spring_6_rest_mvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +17,14 @@ import java.util.UUID;
 @RequestMapping("api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+
+    @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity handlePost(@RequestBody Beer beer) {
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
