3e688a4e64af6302fb39c44fa1fb77ddcd507c00
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-05 16:34:53 +0100

    Spring Boot JPA Test Splice

---
+++ src/test/java/guru_springframework/spring_6_rest_mvc/repositories/BeerRepositoryTest.java
@@ -0,0 +1,25 @@
+package guru_springframework.spring_6_rest_mvc.repositories;
+
+import guru_springframework.spring_6_rest_mvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeer() {
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                .beerName("New Beer")
+                .build());
+
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getId()).isNotNull();
+    }
+}
