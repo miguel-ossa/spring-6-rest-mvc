d03236d6d2d3dabe6afb8a9400942af7581b55d0
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-05 17:14:41 +0100

    MapStruct Mappers

---
+++ src/main/java/guru_springframework/spring_6_rest_mvc/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package guru_springframework.spring_6_rest_mvc.mappers;
+
+import guru_springframework.spring_6_rest_mvc.entities.Beer;
+import guru_springframework.spring_6_rest_mvc.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    Beer beerDtoToBeer(BeerDTO dto);
+
+    Beer beerToBeerDto(Beer beer);
+}
---
+++ src/main/java/guru_springframework/spring_6_rest_mvc/mappers/CustomerMapper.java
@@ -0,0 +1,13 @@
+package guru_springframework.spring_6_rest_mvc.mappers;
+
+import guru_springframework.spring_6_rest_mvc.entities.Customer;
+import guru_springframework.spring_6_rest_mvc.model.CustomerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+
+    Customer customerDtoToCustomer(CustomerDTO dto);
+
+    Customer customerToCustomerDto(Customer customer);
+}
