286266aa2ca9d54cdef2a4a5bfa0a3f1ac48c563
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-06 19:06:09 +0100

    Controller Integration Test

---
+++ src/test/java/guru_springframework/spring_6_rest_mvc/controllers/BeerControllerIT.java
@@ -0,0 +1,39 @@
+package guru_springframework.spring_6_rest_mvc.controllers;
+
+import guru_springframework.spring_6_rest_mvc.model.BeerDTO;
+import guru_springframework.spring_6_rest_mvc.repositories.BeerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BeerControllerIT {
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
