71e98b6839b76c232733c0c32b40e34f4ac1b971
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-08 16:24:58 +0100

    Implementation of Customer Get Operations and IT tests

---
+++ src/test/java/guru_springframework/spring_6_rest_mvc/controllers/CustomerControllerIT.java
@@ -0,0 +1,49 @@
+package guru_springframework.spring_6_rest_mvc.controllers;
+
+import guru_springframework.spring_6_rest_mvc.entities.Customer;
+import guru_springframework.spring_6_rest_mvc.model.CustomerDTO;
+import guru_springframework.spring_6_rest_mvc.repositories.CustomerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class CustomerControllerIT {
+    @Autowired
+    CustomerController customerController;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testCustomerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> customerController.getCustomerById(UUID.randomUUID()));
+    }
+
+    @Test
+    void testGetByID() {
+        Customer customer = customerRepository.findAll().getFirst();
+
+        CustomerDTO dto = customerController.getCustomerById(customer.getId());
+
+        assertThat(dto).isNotNull();
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        customerRepository.deleteAll();
+        List<CustomerDTO> dtos = customerController.listCustomers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
