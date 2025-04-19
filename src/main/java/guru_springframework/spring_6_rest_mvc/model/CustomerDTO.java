d4709c617f9757c23ed6b8d190a6ceb8ad363bd8
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-19 17:15:39 +0100

    Fixed CustomerDTO and Run Configuration

--- src/main/java/guru_springframework/spring_6_rest_mvc/model/CustomerDTO.java
+++ src/main/java/guru_springframework/spring_6_rest_mvc/model/CustomerDTO.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 public class CustomerDTO {
     private UUID id;
     private String customerName;
+    private String email;
     private Integer version;
     private LocalDateTime createdDate;
     private LocalDateTime lastModifiedDate;
