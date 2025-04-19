c06e62c30d1eed0be75daf06ef50a3e7e7148ae0
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-19 15:29:16 +0100

    Add Database Column

--- src/main/java/guru_springframework/spring_6_rest_mvc/entities/Customer.java
+++ src/main/java/guru_springframework/spring_6_rest_mvc/entities/Customer.java
@@ -24,6 +24,9 @@ public class Customer {
     private UUID id;
     private String customerName;
 
+    @Column(length = 255)
+    private String email;
+
     @Version
     private Integer version;
 
