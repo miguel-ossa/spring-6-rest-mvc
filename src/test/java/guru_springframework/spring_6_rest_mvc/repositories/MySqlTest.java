dd523272a0c372c2019b72726e0e429e90437115
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-20 17:09:12 +0100

    Using Service Conn with Test Containers

--- src/test/java/guru_springframework/spring_6_rest_mvc/repositories/MySqlTest.java
+++ src/test/java/guru_springframework/spring_6_rest_mvc/repositories/MySqlTest.java
@@ -4,9 +4,8 @@ import guru_springframework.spring_6_rest_mvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -21,14 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MySqlTest {
 
     @Container
+    @ServiceConnection
     static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8");
-
-    @DynamicPropertySource
-    static void mySqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-    }
 
     @Autowired
     BeerRepository beerRepository;
