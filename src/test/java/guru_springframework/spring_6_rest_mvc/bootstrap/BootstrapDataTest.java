6c4408c44f13e52658e6b7ecaddffdf0ea648331
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-21 12:16:49 +0100

    Fix BootstrapDataTest to find the lost bean using JPA Test

--- src/test/java/guru_springframework/spring_6_rest_mvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru_springframework/spring_6_rest_mvc/bootstrap/BootstrapDataTest.java
@@ -3,15 +3,17 @@ package guru_springframework.spring_6_rest_mvc.bootstrap;
 import guru_springframework.spring_6_rest_mvc.repositories.BeerRepository;
 import guru_springframework.spring_6_rest_mvc.repositories.CustomerRepository;
 import guru_springframework.spring_6_rest_mvc.services.BeerCsvService;
+import guru_springframework.spring_6_rest_mvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@DataJpaTest
-@SpringBootTest
+@DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
     @Autowired
