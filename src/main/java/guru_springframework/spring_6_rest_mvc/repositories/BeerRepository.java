cdc37d00371d2e6189d8c281cff0d82c31423522
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-22 10:18:44 +0100

    Find By Name with Spring Data JPA

--- src/main/java/guru_springframework/spring_6_rest_mvc/repositories/BeerRepository.java
+++ src/main/java/guru_springframework/spring_6_rest_mvc/repositories/BeerRepository.java
@@ -3,7 +3,10 @@ package guru_springframework.spring_6_rest_mvc.repositories;
 import guru_springframework.spring_6_rest_mvc.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
--- src/test/java/guru_springframework/spring_6_rest_mvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru_springframework/spring_6_rest_mvc/repositories/BeerRepositoryTest.java
@@ -1,22 +1,34 @@
 package guru_springframework.spring_6_rest_mvc.repositories;
 
+import guru_springframework.spring_6_rest_mvc.bootstrap.BootstrapData;
 import guru_springframework.spring_6_rest_mvc.entities.Beer;
 import guru_springframework.spring_6_rest_mvc.model.BeerStyle;
+import guru_springframework.spring_6_rest_mvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@Import({BootstrapData.class, BeerCsvServiceImpl.class})
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerListByName() {
+        List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(list.size()).isGreaterThan(30);
+    }
 
     @Test
     void testSaveBeerNameTooLong() {
