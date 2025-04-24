99508b9f1ad774813b456f84d680d11615a9864b
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-04-24 10:44:48 +0100

    One to Many Bidirectional

--- src/main/java/guru_springframework/spring_6_rest_mvc/entities/Customer.java
+++ src/main/java/guru_springframework/spring_6_rest_mvc/entities/Customer.java
@@ -1,12 +1,18 @@
 package guru_springframework.spring_6_rest_mvc.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -22,6 +28,11 @@ public class Customer {
     @JdbcTypeCode(SqlTypes.CHAR)
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
+
+    @NotBlank
+    @NotNull
+    @Size(max = 50)
+    @Column(length = 50)
     private String customerName;
 
     @Column(length = 255)
@@ -30,6 +41,13 @@ public class Customer {
     @Version
     private Integer version;
 
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    @UpdateTimestamp
     private LocalDateTime lastModifiedDate;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<BeerOrder> beerOrders;
 }
