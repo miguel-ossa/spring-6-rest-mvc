6a03273da04517f0f709c5986271b397c857cad7
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-05-21 11:56:45 +0100

    Configure Swagger Request Validator

--- src/test/java/guru_springframework/spring_6_rest_mvc/controllers/BeerControllerRestAssuredTest.java
+++ src/test/java/guru_springframework/spring_6_rest_mvc/controllers/BeerControllerRestAssuredTest.java
@@ -1,5 +1,7 @@
 package guru_springframework.spring_6_rest_mvc.controllers;
 
+import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import com.atlassian.oai.validator.restassured.OpenApiValidationFilter;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +23,10 @@ import static io.restassured.RestAssured.given;
 @Import(BeerControllerRestAssuredTest.TestConfig.class)
 @ComponentScan(basePackages = "guru_springframework.spring_6_rest_mvc")
 public class BeerControllerRestAssuredTest {
+
+    OpenApiValidationFilter filter = new OpenApiValidationFilter(OpenApiInteractionValidator
+            .createForSpecificationUrl("oa3.yml")
+            .build());
 
     @Configuration
     public static class TestConfig {
@@ -45,6 +51,7 @@ public class BeerControllerRestAssuredTest {
     void testListBeers() {
         given().contentType(ContentType.JSON)
                 .when()
+                .filter(filter)
                 .get("/api/v1/beer")
                 .then()
                 .assertThat().statusCode(200);
