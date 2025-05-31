d966846dc8a24debf011afc7c68f27a6c888a855
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-05-31 19:06:47 +0100

    Add Cache Dependencies and Config

--- src/main/java/guru_springframework/spring_6_rest_mvc/Spring6RestMvcApplication.java
+++ src/main/java/guru_springframework/spring_6_rest_mvc/Spring6RestMvcApplication.java
@@ -2,7 +2,9 @@ package guru_springframework.spring_6_rest_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class Spring6RestMvcApplication {
 
