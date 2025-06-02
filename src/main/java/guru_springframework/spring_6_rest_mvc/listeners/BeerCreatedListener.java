e274994274076a3464132af42894c523ac68d230
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-06-02 15:41:12 +0100

    Asyncrhronous Event Processing

--- src/main/java/guru_springframework/spring_6_rest_mvc/listeners/BeerCreatedListener.java
+++ src/main/java/guru_springframework/spring_6_rest_mvc/listeners/BeerCreatedListener.java
@@ -2,15 +2,20 @@ package guru_springframework.spring_6_rest_mvc.listeners;
 
 import guru_springframework.spring_6_rest_mvc.events.BeerCreatedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BeerCreatedListener {
 
+    @Async
     @EventListener
     public void listen(BeerCreatedEvent event) {
         System.out.println("I heard a beer was created!");
         System.out.println(event.getBeer().getId());
+
+        System.out.println("Current Thread Name: " + Thread.currentThread().getName());
+        System.out.println("Current Thread ID: " + Thread.currentThread().getId());
 
         //todo impl - add real implementation to persist audit record
     }
