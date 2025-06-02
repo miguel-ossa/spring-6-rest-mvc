f6127f9c1b4f800daf29163e5ba1e17abfa7abe1
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-06-02 11:52:30 +0100

    Create Application Event Pojos

---
+++ src/main/java/guru_springframework/spring_6_rest_mvc/events/BeerCreatedEvent.java
@@ -0,0 +1,19 @@
+package guru_springframework.spring_6_rest_mvc.events;
+
+import guru_springframework.spring_6_rest_mvc.entities.Beer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.Authentication;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class BeerCreatedEvent {
+
+    private Beer beer;
+
+    private Authentication authentication;
+}
