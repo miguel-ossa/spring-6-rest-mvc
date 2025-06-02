b8d5b23ba79beec07dc0df34a0f04fe4062dc358
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-06-02 16:20:29 +0100

    Persist Audit Event

--- src/main/java/guru_springframework/spring_6_rest_mvc/listeners/BeerCreatedListener.java
+++ src/main/java/guru_springframework/spring_6_rest_mvc/listeners/BeerCreatedListener.java
@@ -1,22 +1,35 @@
 package guru_springframework.spring_6_rest_mvc.listeners;
 
 import guru_springframework.spring_6_rest_mvc.events.BeerCreatedEvent;
+import guru_springframework.spring_6_rest_mvc.mappers.BeerMapper;
+import guru_springframework.spring_6_rest_mvc.repositories.BeerAuditRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class BeerCreatedListener {
+
+    private final BeerMapper beerMapper;
+    private final BeerAuditRepository beerAuditRepository;
 
     @Async
     @EventListener
     public void listen(BeerCreatedEvent event) {
-        System.out.println("I heard a beer was created!");
-        System.out.println(event.getBeer().getId());
 
-        System.out.println("Current Thread Name: " + Thread.currentThread().getName());
-        System.out.println("Current Thread ID: " + Thread.currentThread().getId());
+        val beerAudit = beerMapper.beerToBeerAudit(event.getBeer());
+        beerAudit.setAuditEventType("BEER_CREATED");
 
-        //todo impl - add real implementation to persist audit record
+        if (event.getAuthentication() != null && event.getAuthentication().getName() != null) {
+            beerAudit.setPrincipalName(event.getAuthentication().getName());
+        }
+
+        val savedBeerAudit = beerAuditRepository.save(beerAudit);
+        log.debug("Beer Audit Saved: " + savedBeerAudit.getId());
     }
 }
---
+++ src/main/java/guru_springframework/spring_6_rest_mvc/repositories/BeerAuditRepository.java
@@ -0,0 +1,9 @@
+package guru_springframework.spring_6_rest_mvc.repositories;
+
+import guru_springframework.spring_6_rest_mvc.entities.BeerAudit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerAuditRepository extends JpaRepository<BeerAudit, UUID> {
+}
